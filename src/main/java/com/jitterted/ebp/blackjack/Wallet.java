282fd9c5f4b4c1cadf4c427e36224704d779d93d
Ted M. Young <tedyoung@gmail.com>
2022-02-16 14:36:33 -0800

    TDD: Refactored: removed duplication of isEmpty, replaced with balance == 0

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -2,25 +2,23 @@ package com.jitterted.ebp.blackjack;
 
 public class Wallet {
 
-    private boolean isEmpty;
     private int balance;
 
     public Wallet() {
-        isEmpty = true;
         balance = 0;
     }
 
     public boolean isEmpty() {
-        return isEmpty;
+        return balance == 0;
     }
 
     public void addMoney(int amount) {
-        isEmpty = false;
         balance += amount;
     }
 
     public int balance() {
         return balance;
     }
+
 }
 
